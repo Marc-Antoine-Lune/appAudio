2467a27de6076d205ba50b022ca74f44fe8c5269
Marc-Antoine-Lune <41789916+Marc-Antoine-Lune@users.noreply.github.com>
2019-08-24 13:28:20 +0200

    add popup modal and minor change

--- app.js
+++ app.js
@@ -8,6 +8,39 @@ var test = 0;
 var id;
 var urlApi = 'https://appaudio1.herokuapp.com';
 
+function initPop() {
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function showModal() {
+    document.getElementById('myModal').style.display = "block";
+
+}
+
+
 
 // Fonction d'initialisation de la carte
 function initMap() {
@@ -23,6 +56,7 @@ function initMap() {
 window.onload = function() {
     // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
     initMap();
+    initPop();
 };
 
 
