d8ee82a46bd8d333a1ee473f98d563918f089459
Marc-Antoine-Lune <41789916+Marc-Antoine-Lune@users.noreply.github.com>
2019-08-24 13:49:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -11,18 +11,10 @@ var urlApi = 'https://appaudio1.herokuapp.com';
 function initPop() {
     var modal = document.getElementById("myModal");
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
 
-    // When the user clicks on <span> (x), close the modal
+
     span.onclick = function() {
         modal.style.display = "none";
     }
